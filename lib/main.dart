e89170c45d365e14238bc63d3d4524f32d5bd1c0
VirvaInfortechPC1 <virvainfotech@gmail.com>
2022-09-27 15:31:51 +0530

    forground notification

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-
 import 'dart:convert';
 
 import 'package:firebase_messaging/firebase_messaging.dart';
@@ -7,59 +6,12 @@ import 'package:http/http.dart' as http;
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:firebase_core/firebase_core.dart';
 
-Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
-  await Firebase.initializeApp();
-  await setupFlutterNotifications();
-}
 
-late AndroidNotificationChannel channel;
-
-bool isFlutterLocalNotificationsInitialized = false;
-
-
-Future<void> setupFlutterNotifications() async {
-  if (isFlutterLocalNotificationsInitialized) {
-    return;
-  }
-  channel =  const AndroidNotificationChannel(
-    "0" ,// id
-    'notifaction', // title
-    description: 'This channel is used for important notifications.', // description
-    importance: Importance.high,
-  );
-
-  var flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
-
-  await flutterLocalNotificationsPlugin
-      .resolvePlatformSpecificImplementation<
-      AndroidFlutterLocalNotificationsPlugin>()
-      ?.createNotificationChannel(channel);
-
-  await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
-    alert: true,
-    badge: true,
-    sound: true,
-  );
-
-  FirebaseMessaging.onMessage.listen((RemoteMessage message) async{
-    print("notiicaton----$message");
-  });
-
-  FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) async{
-    print("notiicaton----$message");
-  });
-  isFlutterLocalNotificationsInitialized = true;
-}
-
-
-
-void main()async {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
   runApp(Application());
 }
-
 
 class Application extends StatefulWidget {
   @override
@@ -68,79 +20,42 @@ class Application extends StatefulWidget {
 
 class _Application extends State<Application> {
 
-
-/*
-  Future<void> sendPushMessage() async {
-    try {
-      String? token = await FirebaseMessaging.instance.getAPNSToken();
-
-      await http.post(
-        Uri.parse('https://api.rnfirebase.io/messaging/send'),
-        headers: <String, String>{
-          'Content-Type': 'application/json; '
-              'charset=UTF-8',
-          'Authorization': ' $token',
-
-        },
-        body: constructFCMPayload(_token),
-      );
-      print('FCM request for device sent!');
-    } catch (e) {
-      print(e);
-    }
+  @override
+  void initState() {
+    super.initState();
+    NotificationListenerProvider().getMessage(context);
   }
-*/
 
 
-  void getToken()async{
-    // token = FirebaseMessaging.instance.getAPNSToken() as String?;
+  void sendNotification() async {
     var token = await FirebaseMessaging.instance.getToken();
-    print("token :---------$token");
-   /* var headers = <String,String>{
-      "Content-Type":"application/json",
-      "Authorization":"key=AAAAiTaGLBE:APA91bEg909LqsKH9KtWJe2p541lUR9Yg0vOueXlWP1Szb3B8uiw4nTjiiIi079xsRl3bKZU6zvuclqtdwZQNYslCRJYS665d2hOuUt61soVI9mODkQVoDZpB2ZxRmLQI4udwIgssLls"
-    };
-    var body = <String, dynamic>{
-      "to" :"$token",
-      "notification":{
-        "data" : "hello",
-        "title":"Notification Demo"
-      }
-    };
-    
-    var response = await http.post(Uri.parse("https://fcm.googleapis.com/fcm/send"),
-        headers:headers,
-        body:jsonEncode(body)
+    String? title = "hello";
+    String? body = "Notification";
 
-    );*/
-
-    Future.delayed(const Duration(seconds: 10),() async{
-      var response = await http.post(Uri.parse
-        ( 'https://fcm.googleapis.com/fcm/send'),
-        headers: <String, String>{
-          'Content-Type': 'application/json',
-          'Authorization': 'key=AAAAiTaGLBE:APA91bEg909LqsKH9KtWJe2p541lUR9Yg0vOueXlWP1Szb3B8uiw4nTjiiIi079xsRl3bKZU6zvuclqtdwZQNYslCRJYS665d2hOuUt61soVI9mODkQVoDZpB2ZxRmLQI4udwIgssLls',
-        },
-        body: jsonEncode(
-          <String, dynamic>{
-            "notification": <String, dynamic>{
-              "body": "notifiva6oppb", // New artical has been post in EventChannel
-              "title": "hello"
-            },
-            "priority": "normal",
-            "to": "$token",
+    Future.delayed(
+      const Duration(seconds: 10),
+      () async {
+         await http.post(
+          Uri.parse('https://fcm.googleapis.com/fcm/send'),
+          headers: <String, String>{
+            'Content-Type': 'application/json',
+            'Authorization':
+                'key=AAAAiTaGLBE:APA91bEg909LqsKH9KtWJe2p541lUR9Yg0vOueXlWP1Szb3B8uiw4nTjiiIi079xsRl3bKZU6zvuclqtdwZQNYslCRJYS665d2hOuUt61soVI9mODkQVoDZpB2ZxRmLQI4udwIgssLls',
           },
-        ),
-      );
-      print("response-----${response.statusCode}");
-      print("response-----${response.body}");
-    },);
-
-
-
-    
+          body: jsonEncode(
+            <String, dynamic>{
+              "notification": <String, dynamic>{
+                "body": body,
+                "title": title
+              },
+              "priority": "normal",
+              "to": "$token",
+            },
+          ),
+        );
+      },
+    );
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -152,10 +67,9 @@ class _Application extends State<Application> {
         body: Center(
           child: ElevatedButton(
             onPressed: () {
-              getToken();
-              FirebaseMessaging.instance.getInitialMessage();
+              sendNotification();
             },
-            child:const Text('send Notification'),
+            child: const Text('send Notification'),
           ),
         ),
       ),
@@ -163,3 +77,50 @@ class _Application extends State<Application> {
   }
 }
 
+void sendNotification( {String? title, String? body}) async {
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+  FlutterLocalNotificationsPlugin();
+  const AndroidInitializationSettings initializationSettingsAndroid =
+  AndroidInitializationSettings('@mipmap/ic_launcher');
+
+
+  const InitializationSettings initializationSettings = InitializationSettings(
+    android: initializationSettingsAndroid,);
+  await flutterLocalNotificationsPlugin.initialize(
+    initializationSettings,);
+
+  ///
+  const AndroidNotificationChannel channel = AndroidNotificationChannel(
+      'high_channel', 'High Importance Notification',
+      description: "This channel is for important notification",
+      importance: Importance.max);
+
+  flutterLocalNotificationsPlugin.show(
+    0,
+    title,
+    body,
+    NotificationDetails(
+      android: AndroidNotificationDetails(
+          channel.id,
+          channel.name,
+          channelDescription: channel.description),
+    ),
+  );
+}
+
+class NotificationListenerProvider {
+  final _firebaseMessaging = FirebaseMessaging.instance.getInitialMessage();
+
+  void getMessage(BuildContext context) {
+    FirebaseMessaging.onMessage.listen((RemoteMessage event) {
+      RemoteNotification notification = event.notification!;
+      AndroidNotification androidNotification = event.notification!.android!;
+
+      if (notification != null && androidNotification != null) {
+
+        sendNotification(title: notification.title!, body: notification.body);
+
+      }
+    });
+  }
+}
